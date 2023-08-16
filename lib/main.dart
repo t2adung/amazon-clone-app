1ef4a5729582c70e9c79e46a19aa2109e6364e0e
Dung Tran <t2adung@gmail.com>
2023-08-17 06:28:21 +0700

    setting router

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
+import 'package:amazon_clone_tutorial/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone_tutorial/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,22 +21,13 @@ class MyApp extends StatelessWidget {
             primary: GlobalVariables.secondaryColor,
           ),
           appBarTheme: const AppBarTheme(
-
             elevation: 0,
             iconTheme: IconThemeData(
               color: Colors.black,
             ),
           )),
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Hello.'),
-          ),
-          body: Column(children: [
-            const Center(
-              child: Text('Flutter Demo Home Page'),
-            ),
-            ElevatedButton(onPressed: () {}, child: const Text('click'))
-          ])),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
     );
   }
 }
